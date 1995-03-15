4bb81d06577a2d72ec80eb03690d113c87c63012
gray <gray>
1995-03-15 00:17:51 +0000

    show program version.

--- test/test.bat
+++ test/test.bat
@@ -1,8 +1,7 @@
-REM	test script for MS-DOS  
 erase test.out
 erase test2.out
 erase test3.out
-gema -f testpat.dat testin.dat test.out
+gema -version -f testpat.dat testin.dat test.out
 fc testout.dat test.out
 gema -f testpat.dat -i -idchars "-_" -out test2.out test2.dat
 fc test2out.dat test2.out
