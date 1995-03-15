5fe903b259590669ff07495775dfd251fc2f34a3
gray <gray>
1995-03-15 00:33:23 +0000

    Update from 0.1 to 0.4.

--- src/version.c
+++ src/version.c
@@ -1,2 +1,15 @@
 
-const char Version [] = "gema 0.1 Feb 26, 1995";
+/* $Id$ */
+
+/*      Version identification string
+
+	The prefix "@(#)" is searched for by the Unix "what" command.
+	The characters following that will also be displayed by the 
+	"-version" option or the "@version" function.
+
+	If you make any changes to this program at all, please be
+	sure to update this string also, so that your modified version
+	can be distinguished from the original version.
+ */
+
+const char what_string [] = "@(#)gema 0.4 March 12, 1995";
