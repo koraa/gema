4adb4039f48a9db58bd15186e7dd816372c152b7
gray <gray>
1996-04-08 05:10:39 +0000

    New `goal_state' structure to allow passing context for goal match.

--- src/pattern.h
+++ src/pattern.h
@@ -13,12 +13,13 @@ Domain get_domain( const char* name );
 
 int read_patterns ( CIStream s, const char* default_domain, boolean undef );
 
+typedef struct goal_state* Goal;
+
 /* Read from `in', translate according to `p', and write the result
    to `out', until the next characters to be read matches `goal'.
    Returns TRUE on success, or FALSE if the translation fails or if
    end-of-file is reached before the goal string is found.  */
-boolean translate ( CIStream in, Domain d, COStream out,
-		    const unsigned char* goal );
+boolean translate ( CIStream in, Domain d, COStream out, Goal goal_info );
 
 extern boolean line_mode;
 extern boolean token_mode;
